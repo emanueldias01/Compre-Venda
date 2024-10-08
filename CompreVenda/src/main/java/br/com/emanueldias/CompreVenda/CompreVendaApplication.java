5d6d87844f2898a8a254c0832c17920ff830ad4c
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-10-08 10:26:44 -0300

    upload de arquivos

---
+++ CompreVenda/src/main/java/br/com/emanueldias/CompreVenda/CompreVendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias.CompreVenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompreVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompreVendaApplication.class, args);
+	}
+
+}
---
+++ CompreVenda/src/test/java/br/com/emanueldias/CompreVenda/CompreVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias.CompreVenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompreVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
