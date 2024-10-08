0d07e07716b4167f6e8ecd10167daff89a6493f2
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-10-08 12:32:34 -0300

    cria controller de pagamento

---
+++ CompreVenda/src/main/java/br/com/emanueldias/CompreVenda/pagamento/controller/PagamentoController.java
@@ -0,0 +1,34 @@
+package br.com.emanueldias.CompreVenda.pagamento.controller;
+
+import br.com.emanueldias.CompreVenda.pagamento.dto.PagamentoRequestDTO;
+import br.com.emanueldias.CompreVenda.pagamento.dto.PagamentoResponseDTO;
+import br.com.emanueldias.CompreVenda.pagamento.service.PagamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pagamentos")
+public class PagamentoController {
+
+    @Autowired
+    PagamentoService pagamentoService;
+
+    @GetMapping
+    public ResponseEntity<List<PagamentoResponseDTO>> listAll(){
+        return ResponseEntity.ok(pagamentoService.listAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<PagamentoResponseDTO> create(@RequestBody PagamentoRequestDTO dto){
+        return ResponseEntity.ok(pagamentoService.create(dto));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity cancelaPagamento(@PathVariable Long id){
+        return ResponseEntity.ok(pagamentoService.cancelaPagamento(id));
+    }
+
+}
