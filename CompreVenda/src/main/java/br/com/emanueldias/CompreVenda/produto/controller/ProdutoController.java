7227b5076455a983ecfb01e510055965368f7944
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-10-08 11:02:03 -0300

    criacao de controller

---
+++ CompreVenda/src/main/java/br/com/emanueldias/CompreVenda/produto/controller/ProdutoController.java
@@ -0,0 +1,40 @@
+package br.com.emanueldias.CompreVenda.produto.controller;
+
+import br.com.emanueldias.CompreVenda.produto.dto.ProdutoRequestDTO;
+import br.com.emanueldias.CompreVenda.produto.dto.ProdutoResponseDTO;
+import br.com.emanueldias.CompreVenda.produto.dto.ProdutoUpdateDTO;
+import br.com.emanueldias.CompreVenda.produto.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoResponseDTO>> getAll(){
+        return ResponseEntity.ok(produtoService.listAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoResponseDTO> create(@RequestBody ProdutoRequestDTO dto){
+        return ResponseEntity.ok(produtoService.create(dto));
+    }
+
+    @PutMapping
+    public ResponseEntity<ProdutoResponseDTO> update(@RequestBody ProdutoUpdateDTO dto){
+        return ResponseEntity.ok(produtoService.update(dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        produtoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
