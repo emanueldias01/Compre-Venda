19633464ff06ca3bdf6aa22cbd6f389ba40a4b42
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-10-10 18:06:05 -0300

    upload de arquivos de microservico de pagamentos

---
+++ CompreVendaMircoServices/pagamentos/src/main/java/br/com/emanueldias/pagamentos/PagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentosApplication.class, args);
+	}
+
+}
---
+++ CompreVendaMircoServices/pagamentos/src/test/java/br/com/emanueldias/pagamentos/PagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
