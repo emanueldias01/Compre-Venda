f85aca2804f5c6b33fa2b7928df47ba17c095af5
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-10-10 19:27:13 -0300

    configura server eureka

--- CompreVendaMircoServices/server/src/main/java/br/com/emanueldias/server/ServerApplication.java
+++ CompreVendaMircoServices/server/src/main/java/br/com/emanueldias/server/ServerApplication.java
@@ -2,8 +2,10 @@ package br.com.emanueldias.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServerApplication {
 
 	public static void main(String[] args) {
