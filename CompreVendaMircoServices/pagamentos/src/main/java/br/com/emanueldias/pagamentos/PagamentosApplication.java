3839c6bb9dde37f4ba53a5af7370a9aeb597951b
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-10-10 19:33:40 -0300

    configura servicos para aparecerem no server discovery

--- CompreVendaMircoServices/pagamentos/src/main/java/br/com/emanueldias/pagamentos/PagamentosApplication.java
+++ CompreVendaMircoServices/pagamentos/src/main/java/br/com/emanueldias/pagamentos/PagamentosApplication.java
@@ -2,8 +2,10 @@ package br.com.emanueldias.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PagamentosApplication {
 
 	public static void main(String[] args) {
--- CompreVendaMircoServices/pedidos/src/main/java/br/com/emanueldias/pedidos/PedidosApplication.java
+++ CompreVendaMircoServices/pedidos/src/main/java/br/com/emanueldias/pedidos/PedidosApplication.java
@@ -2,8 +2,10 @@ package br.com.emanueldias.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PedidosApplication {
 
 	public static void main(String[] args) {
