b30b56eba783b43293e388ccd445bf39fd5a8061
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-10-16 14:21:57 -0300

    configura queue de pedidos criados

---
+++ CompreVendaMircoServices/pedidos/src/main/java/br/com/emanueldias/pedidos/config/AMQPConfig.java
@@ -0,0 +1,29 @@
+package br.com.emanueldias.pedidos.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AMQPConfig {
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public Queue criaQueuePedidosCriados(){
+        return QueueBuilder.nonDurable("pedido.criado").build();
+    }
+}
