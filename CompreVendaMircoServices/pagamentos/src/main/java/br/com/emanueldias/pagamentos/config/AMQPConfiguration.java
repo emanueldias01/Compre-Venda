0d4502dbccb8f7b29eb8d4738022393546241224
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-10-16 09:21:57 -0300

    configura amqp em servico de pagamento

---
+++ CompreVendaMircoServices/pagamentos/src/main/java/br/com/emanueldias/pagamentos/config/AMQPConfiguration.java
@@ -0,0 +1,30 @@
+package br.com.emanueldias.pagamentos.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AMQPConfiguration {
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public Queue criaQueue(){
+        return QueueBuilder.nonDurable("pagamento.concluido").build();
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+}
